75bba833da5c5b2ba5d213dd6b1951820f45c455
Arthur Silva <arthur1silva1r@gmail.com>
2022-02-13 13:47:36 -0300

    fix. imports indevidos

--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -1,6 +1,5 @@
 import { useContext } from 'react';
 import '../style/List.css';
-import { Button } from 'reactstrap';
 import ContextList from '../context/ContextList';
 import Header from '../components/Header';
 import FormTask from '../components/FormTask';
